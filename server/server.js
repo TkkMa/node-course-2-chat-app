28d2fdfc1ebe308584a218dcaf4c6a18e1b9f12a
Terence Ma <tma@gsma.com>
2018-03-07 11:45:05 +0800

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 const socketIO = require('socket.io');
-//const {generateMessage} = require('./utils/message');
+const {generateMessage} = require('./utils/message');
 // console.log(__dirname + '/../public');
 // console.log(publicPath);
 const port = process.env.PORT || 3000;
@@ -16,27 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) =>{
     console.log('New user connected');
 
-    // socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));  // emitting from server -> client
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));  // emitting from server -> client
 
-    // socket.emit('newMessage', generateMessage('Admin', 'New user joined')); // socket.emit emits event to a single connection
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
-    socket.broadcast.emit('newMessage',{
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined')); // socket.emit emits event to a single connection
 
     socket.on('createMessage', (message) =>{  // listen to event sent from client
         console.log('createMessage', message);
-        io.emit('newMessage', {   // emits to every single event
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text))   // emits to every single event
         // socket.broadcast.emit('newMessage',{
         //     from: message.from,
         //     text: message.text,
