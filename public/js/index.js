294767f810b75408c6a6c0c58673e02dbce769df
TkkMa <tma@gsma.com>
2018-03-10 16:42:21 +0800

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -14,9 +14,10 @@ socket.on('disconnect',function () {
 });
 //-- log this to the screen i.e. chrome dev tool console, listening to server emit event
 socket.on('newMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     console.log('newMessage', message);
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     
     jQuery('#messages').append(li);
 });
@@ -29,10 +30,11 @@ socket.on('newMessage', function(message) {
 //     console.log('Got it', data);
 // });
 socket.on('newLocationMessage', function(message){
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');  //_blank opens up a tab to view google maps
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from}${formattedTime}: `);
     a.attr('href', message.url)
     li.append(a);
     jQuery('#messages').append(li);
